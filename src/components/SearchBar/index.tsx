c5a0db57b71baa2020736850f1e9b1cf4cc30f8e
bbosmedia <bbosmedia@users.noreply.github.com>
2022-11-19 17:22:52 +0500

    Prettier added to project to make code readeable

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,31 +1,47 @@
 import React, { useState } from 'react'
-import {FiSearch} from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi'
 import './style.scss'
-import { useAppDispatch } from './../../hooks/redux';
-import { changeSearchTerm } from '../../redux/slices/searchFilter.slice';
-import {ClipLoader} from "react-spinners";
+import { useAppDispatch } from './../../hooks/redux'
+import { changeSearchTerm } from '../../redux/slices/searchFilter.slice'
+import { ClipLoader } from 'react-spinners'
 
-const SearchBar = ({isLoading}: {isLoading:boolean}) => {
-  const [value, setValue] = useState<string>('')
-  const dispatch = useAppDispatch()
-  const search = (e: React.FormEvent<HTMLFormElement>) =>{
-    e.preventDefault()
-    dispatch(changeSearchTerm(value))
-  }
-  return (
-      <form onSubmit={(e)=>search(e)} className='search-bar'>
-        <div className='icon'>
-        <FiSearch />
-      </div>
-        <input type="text" value={value} onChange={e=> setValue(e.target.value)} placeholder='Plaats, buurt, adres, etc.' />
-        <button disabled={isLoading} className='search-bar-btn'>
-          {isLoading ?<ClipLoader color="#fff" size={15} /> :<span>Search</span>}
-        </button>
-        <button disabled={isLoading} className='search-bar-btn-mobile'>
-          {isLoading ?<ClipLoader color="#fff" size={15} /> :<span><FiSearch /></span>}
-        </button>
-      </form>
-  )
+const SearchBar = ({ isLoading }: { isLoading: boolean }) => {
+	const [value, setValue] = useState<string>('')
+	const dispatch = useAppDispatch()
+	const search = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		dispatch(changeSearchTerm(value))
+	}
+	return (
+		<form onSubmit={e => search(e)} className='search-bar'>
+			<div className='icon'>
+				<FiSearch />
+			</div>
+			<input
+				type='text'
+				className='searchbar-input'
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				placeholder='Plaats, buurt, adres, etc.'
+			/>
+			<button disabled={isLoading} className='search-bar-btn'>
+				{isLoading ? (
+					<ClipLoader color='#fff' size={15} />
+				) : (
+					<span>Search</span>
+				)}
+			</button>
+			<button disabled={isLoading} className='search-bar-btn-mobile'>
+				{isLoading ? (
+					<ClipLoader color='#fff' size={15} />
+				) : (
+					<span>
+						<FiSearch />
+					</span>
+				)}
+			</button>
+		</form>
+	)
 }
 
 export default SearchBar
--- src/components/SideBarTool/index.tsx
+++ src/components/SideBarTool/index.tsx
@@ -1,45 +1,105 @@
 import React, { useState } from 'react'
-import {FaCaretDown} from 'react-icons/fa'
+import { FaCaretDown } from 'react-icons/fa'
 import './style.scss'
-import { useAppDispatch, useAppSelector } from './../../hooks/redux';
-import { changeSort, changeType } from '../../redux/slices/searchFilter.slice';
+import { useAppDispatch, useAppSelector } from './../../hooks/redux'
+import { changeSort, changeType } from '../../redux/slices/searchFilter.slice'
 
 const SideBarTools = () => {
-  const dispatch = useAppDispatch()
-  const filter = useAppSelector(state => state.searchFilter)
-  const [openbox, setOpenBox] = useState('')
-  const changeSortBy = (value: string) =>{
-    dispatch(changeSort(value))
-    setOpenBox('')
-  }
-  const changeTypeBy = (value: string) =>{
-    dispatch(changeType(value))
-    setOpenBox('')
-  }
-  return (
-    <aside className='sidebar-tools'>
-      <div className='sort select-box'>
-        <div className="top"><span>Sort by</span> <button onClick={()=>setOpenBox('Sort by')}><span>{filter.sortby}</span> <span className='icon'>
-          <FaCaretDown />
-          </span></button></div>
-        <div className={openbox === 'Sort by' ? 'body active': 'body'}>
-          <button className={filter.sortby === 'Date Added' ? 'select-btn active': 'select-btn'} onClick={()=>changeSortBy('Date Added')}>Date Added</button>
-          <button className={filter.sortby === 'Highest Price' ? 'select-btn active': 'select-btn'} onClick={()=>changeSortBy('Highest Price')}>Highest Price</button>
-          <button className={filter.sortby === 'Lowest Price' ? 'select-btn active': 'select-btn'} onClick={()=>changeSortBy('Lowest Price')}>Lowest Price</button>
-        </div>
-      </div>
-      <div className='sort select-box'>
-        <div className="top"><span>Type</span> <button onClick={()=>setOpenBox('Type')}><span>{filter.type}</span> <span className='icon'>
-          <FaCaretDown />
-          </span></button></div>
-        <div className={openbox === 'Type' ? 'body active': 'body'}>
-          <button className={filter.type === 'Apartment' ? 'select-btn active': 'select-btn'} onClick={()=>changeTypeBy('Apartment')}>Apartment</button>
-          <button className={filter.type === 'Villa' ? 'select-btn active': 'select-btn'} onClick={()=>changeTypeBy('Villa')}>Villa</button>
-          <button className={filter.type === 'Town House' ? 'select-btn active': 'select-btn'} onClick={()=>changeTypeBy('Town House')}>Town House</button>
-        </div>
-      </div>
-    </aside>
-  )
+	const dispatch = useAppDispatch()
+	const filter = useAppSelector(state => state.searchFilter)
+	const [openbox, setOpenBox] = useState('')
+	const changeSortBy = (value: string) => {
+		dispatch(changeSort(value))
+		setOpenBox('')
+	}
+	const changeTypeBy = (value: string) => {
+		dispatch(changeType(value))
+		setOpenBox('')
+	}
+	return (
+		<aside className='sidebar-tools'>
+			<div className='sort select-box'>
+				<div className='top'>
+					<span>Sort by</span>
+					<button onClick={() => setOpenBox('Sort by')}>
+						<span>{filter.sortby}</span>
+						<span className='icon'>
+							<FaCaretDown />
+						</span>
+					</button>
+				</div>
+				<div className={openbox === 'Sort by' ? 'body active' : 'body'}>
+					<button
+						className={
+							filter.sortby === 'Date Added'
+								? 'select-btn active'
+								: 'select-btn'
+						}
+						onClick={() => changeSortBy('Date Added')}
+					>
+						Date Added
+					</button>
+					<button
+						className={
+							filter.sortby === 'Highest Price'
+								? 'select-btn active'
+								: 'select-btn'
+						}
+						onClick={() => changeSortBy('Highest Price')}
+					>
+						Highest Price
+					</button>
+					<button
+						className={
+							filter.sortby === 'Lowest Price'
+								? 'select-btn active'
+								: 'select-btn'
+						}
+						onClick={() => changeSortBy('Lowest Price')}
+					>
+						Lowest Price
+					</button>
+				</div>
+			</div>
+			<div className='sort select-box'>
+				<div className='top'>
+					<span>Type</span>
+					<button onClick={() => setOpenBox('Type')}>
+						<span>{filter.type}</span>
+						<span className='icon'>
+							<FaCaretDown />
+						</span>
+					</button>
+				</div>
+				<div className={openbox === 'Type' ? 'body active' : 'body'}>
+					<button
+						className={
+							filter.type === 'Apartment' ? 'select-btn active' : 'select-btn'
+						}
+						onClick={() => changeTypeBy('Apartment')}
+					>
+						Apartment
+					</button>
+					<button
+						className={
+							filter.type === 'Villa' ? 'select-btn active' : 'select-btn'
+						}
+						onClick={() => changeTypeBy('Villa')}
+					>
+						Villa
+					</button>
+					<button
+						className={
+							filter.type === 'Town House' ? 'select-btn active' : 'select-btn'
+						}
+						onClick={() => changeTypeBy('Town House')}
+					>
+						Town House
+					</button>
+				</div>
+			</div>
+		</aside>
+	)
 }
 
 export default SideBarTools
